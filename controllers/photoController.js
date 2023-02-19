064cfa4235985879fa4969dfb1882ecf495ab005
Michael <michaelkorostilev@gmail.com>
2023-02-19 12:57:19 +0200

    Part III - Advanced Routes: Return Combined Data

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -33,4 +33,34 @@ const getPhotoById = expressAsyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { getPhotos, getPhotoById };
+// example user: susangkomen3day
+const getPhotosByUser = expressAsyncHandler(async (req, res) => {
+  const { username } = req.params;
+  try {
+    const { data } = await axios.get(
+      `${BASE_URL}/users/${username}/photos?client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+    );
+    const user = data.map((photo) => {
+      return {
+        id: photo.id,
+        username: photo.user.username,
+        description: photo?.description || "No description provided.",
+        url: photo.urls.raw,
+      };
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    if (error.response) {
+      res
+        .status(error.response.status)
+        .json({ message: error.response.data.errors });
+    } else if (error.request) {
+      // The request was made but no response was received
+      res.status(500).json({ message: error.request });
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      res.status(500).json({ message: error.message });
+    }
+  }
+});
+module.exports = { getPhotos, getPhotoById, getPhotosByUser };
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getPhotos, getPhotoById } = require("../controllers/photoController");
+const {
+  getPhotos,
+  getPhotoById,
+  getPhotosByUser,
+} = require("../controllers/photoController");
 
 const router = express.Router();
 
 router.get("/", getPhotos);
 router.get("/:id", getPhotoById);
+router.get("/user/:username", getPhotosByUser);
 
 module.exports = router;
